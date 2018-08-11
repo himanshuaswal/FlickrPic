992534fbc40c363e3919610f4c8ad2f56fbc63d7
Gautam Krishnan <gautamkrishnan7209@gmail.com>
2018-08-11 21:37:54 +0530

    Added bottom navigation view

--- app/src/main/java/com/atg/gautam/flickerpic/ui/MainActivity.java
+++ app/src/main/java/com/atg/gautam/flickerpic/ui/MainActivity.java
@@ -1,15 +1,31 @@
 package com.atg.gautam.flickerpic.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
 
 import com.atg.gautam.flickerpic.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private BottomNavigationView mBottomNavigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mBottomNavigationView = findViewById(R.id.bottom_navigation);
+        mBottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.recent_images:
+                    //do something;
+                    break;
+                case R.id.search_images:
+                    //do something;
+                    break;
+
+            }
+            return true;
+        });
     }
 }
